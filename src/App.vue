96f30f561ba847c3a31acc5ccf89dea8e2b555b8
Jason Collum <jasoncollum@gmail.com>
2022-06-08 11:24:59 -0500

    adds basic header setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-  </nav>
-  <router-view/>
+  <Header />
+  <router-view />
 </template>
 
+<script>
+import Header from './components/Header.vue'
 
+export default {
+  name: 'App',
+  components: {Header},
+}
+</script>
 
 <style>
 #app {
@@ -14,18 +19,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
