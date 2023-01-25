98f9bb438e6f1c960fea4ff21bc711a73c697b73
Jason Collum <jasoncollum@gmail.com>
2023-01-25 11:46:45 -0600

    adds ArtistDetail type, adds Create Song Later link to CreateSong, adds create-new-artist select option and handleCreateNewArtist method to redirect to CreateArtist

--- src/views/CreateSong.vue
+++ src/views/CreateSong.vue
@@ -1,23 +1,102 @@
 <template>
-  <div>
-    This is the Create Song View ...
-  </div>
-  <div>{{newArtistId}}</div>
-  <div>{{previousPath}}</div>
+  <!-- Create New Song -->
+  <form @submit.prevent="handleCreateSong">
+    <div class="has-text-centered">Create A New Song</div>
+
+    <label>Artist:</label>
+    <select v-model="selectedArtistId" required @change="handleNewArtist">
+      <option text='Choose an artist' value='' disabled></option>
+      <option text='Create a new artist' value='create-new-artist'></option>
+      <option v-for="artist in artistList" :value="artist.id" :key="artist.id">
+        {{ artist.name }}
+      </option>
+    </select>
+
+    <label>Song Title:</label>
+    <input type="text" v-model="title" required />
+
+    <div class="submit">
+      <button>Create Song</button>
+    </div>
+  </form>
+  <p>{{ this.previousPath}}</p>
+  <div class="is-size-7 has-text-centered">
+      <router-link to="/song-view" class="link" @click="handleCreateSongLater">
+        Create A Song Later
+      </router-link>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ArtistDetail from '@/types/ArtistDetail';
+
+const axios = require('axios').default;
 
 export default defineComponent({
   name: 'CreateSong',
-  computed: {
-    newArtistId() {
-      return this.$store.state.newArtistId
+  async mounted() {
+    await this.getArtistsDetails();
+    this.selectedArtistId = this.$store.state.newArtistId;
+  },
+  data() {
+    return {
+      title: '',
+      selectedArtistId: '',
+      artistList: [] as ArtistDetail[],
+      previousPath: this.$router.options.history.state,
+    }
+  },
+  // computed: {
+
+  // },
+  methods: {
+    async getArtistsDetails() {
+      // get artist details to populate select options
+      try {
+        const token = localStorage.getItem('token');
+        const response = await axios.get(`${process.env.VUE_APP_ROOT_API}/artists/details`, {
+            headers: {
+              'Authorization': `Bearer ${token}`
+            }
+          }
+        )
+        this.artistList = response.data;
+      } catch (error: any) {
+        console.log(error.response.data.message)
+      }
     },
-    previousPath() {
-      console.log(this.$router.options.history.state)
-      return this.$router.options.history.state
+    async handleCreateSong() {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await axios.post(`${process.env.VUE_APP_ROOT_API}/songs`, {
+          title: this.title,
+          artistId: this.selectedArtistId,
+          isOpen: true
+        },
+        {
+            headers: {
+              'Authorization': `Bearer ${token}`
+            }
+          }
+        )
+        this.$store.dispatch('updateNewArtistId', '')
+        this.title = ''
+        this.selectedArtistId = ''
+        this.artistList = []
+        this.$router.push('/song-view')
+      } catch (error: any) {
+        console.log(error.response.data.message)
+      }
+    },
+    handleNewArtist() {
+      if (this.selectedArtistId === 'create-new-artist') {
+        this.$store.dispatch('updateNewArtistId', '');
+        this.$router.push('/create-artist');
+      }
+    },
+    handleCreateSongLater() {
+      this.$store.dispatch('updateNewArtistId', '');
     }
   }
 });
