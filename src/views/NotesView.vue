518b6ebb997722aa1f479ad12eba9266f6671bca
Jason Collum <jasoncollum@gmail.com>
2023-02-02 21:00:59 -0600

    adds conditional rendering if notes do not exist, adds Note component and passes props

--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -6,8 +6,18 @@
       </div>
       <p class="has-text-centered">Client Notes</p>
     </div>
-    <div id="notes-container">
-      <p>Vocals seem like they could come up a little bit overall</p>
+    <div id="notes-container" v-if="hasNotes">
+      <Note 
+        v-for="note in version.notes"
+        :key="note.id"
+        :id="note.id"
+        :text="note.text"
+        :revisions="note.revisions"
+        :versionId="note.versionId"
+      />
+    </div>
+    <div v-else class="has-text-centered">
+      There are currently no notes for this mix version...
     </div>
   </div>
 </template>
@@ -16,13 +26,18 @@
 import { defineComponent } from 'vue';
 import SongWithArtist from '@/types/SongWithArtist';
 import Version from '@/types/Version';
+import Note from '../components/Note.vue'
 
 export default defineComponent({
   name: 'NotesView',
+  components: {
+    Note,
+  },
   data() {
     return {
       versionId: this.$route.params.id as String,
-      version: null as Version | null
+      version: null as Version | null,
+      hasNotes: false, 
     }
   },
   computed: {
@@ -32,6 +47,7 @@ export default defineComponent({
   },
   mounted() {
     this.version = this.song.versions.find(version => version.id === this.versionId) as Version;
+    this.hasNotes = this.version.notes.length > 0;
   }
 });
 </script>
